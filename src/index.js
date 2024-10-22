a1492818fa538a84fd60c4410484726b9767f3ff
nadiahusarova <ngusarova911@gmail.com>
2024-10-22 15:37:08 +0300

    initial prjkt& done 1,2,3 steps

---
+++ src/index.js
@@ -0,0 +1,3 @@
+// const message = "Hello virus";
+
+// console.log(message);
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,21 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const contacts = await readContacts();
+
+    for (let i = 0; i < number; i++) {
+      const newContact = createFakeContact();
+      contacts.push(newContact);
+    }
+
+    await writeContacts(contacts);
+    console.log(`${number} contacts have been added!`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 generateContacts(5);
